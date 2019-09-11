5876c8d906cb64601bf80929c9d3ab7f9721511d
Strykar1 <ahmedms.ftw@gmail.com>
2019-09-11 22:00:15 +0200

    manual merge from feat/camera/main

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private FloatingJoystick joystick;
+    [SerializeField] private float speed = 0.12f;
+
+    private Rigidbody Rigidbody;
+
+    // Start is called before the first frame update
+    void Start() {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Rigidbody.velocity = new Vector3(-joystick.Horizontal * 100f * speed,
+            Rigidbody.velocity.y,
+            -joystick.Vertical * 100f * speed);
+    }
+}
