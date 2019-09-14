16793679523dd08ca425cda85dec1bdebac4d326
RamezIOPhobia <rameznoshy@gmail.com>
2019-09-14 01:30:10 +0000

    add firing system, projectiles
    add basic auto targeting to nearest enemy
    add projectiles that shoot as long as the screen is not pressed
    add rider files
    
    known issues:
        move targeting logic to fireSystem
        wont shoot if screen is pressed and staying still

---
+++ Assets/Scripts/playerFiring/FiringSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace playerFiring
+{
+    public class FiringSystem : MonoBehaviour
+    {
+        [SerializeField] private GameObject projectile = null;
+
+        [SerializeField] private double fireRate = 1000;
+        [SerializeField] private double movePenalty = 0.33;
+        private double nextFire;
+
+        private void Update()
+        {
+            if (nextFire > Time.time) return;
+
+            //todo confirm these r the correct btns
+            if (Input.GetButton("Fire1") || Input.GetButton("Fire2"))
+            {
+                nextFire += movePenalty;
+                return;
+            }
+        
+            Instantiate(projectile, gameObject.transform.position, transform.rotation);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
---
+++ Assets/Scripts/playerFiring/MoveProjectile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace playerFiring
+{
+    public class MoveProjectile : MonoBehaviour
+    {
+
+        [SerializeField] private new Rigidbody rigidbody = null;
+        [SerializeField] private float bulletSpeed = 10;
+        [SerializeField] private GameObject[] enemies;
+        public GameObject target;
+
+        private Vector3 direction;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            enemies = GameObject.FindGameObjectsWithTag("enemy");
+            var startingPosition = gameObject.transform.position;
+        
+            Array.Sort(enemies, (x, y) => 
+                (int) (Vector3.Distance(x.transform.position, startingPosition) -
+                       Vector3.Distance(y.transform.position, startingPosition)));
+
+            // todo check if no enemies in range dont create elbta3 elli ana feh now
+            try
+            {
+                target = enemies[0];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            var targetPosition = target.transform.position;
+            direction = targetPosition - startingPosition;
+            var distance = Vector3.Distance(targetPosition, startingPosition);
+            LayerMask mask = LayerMask.GetMask("enemy");
+            for (int i = 1; i < enemies.Length; i++)
+            {
+                if (Physics.Raycast(startingPosition, direction, distance, ~mask))
+                {
+                    target = enemies[i];
+                    direction = target.transform.position - startingPosition;
+                    distance = Vector3.Distance(target.transform.position, startingPosition);   
+                }
+            }
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            rigidbody.AddForce((direction) * bulletSpeed);
+        }
+    }
+}
