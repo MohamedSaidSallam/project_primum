2fe4b2a1f69d46231e1cc02d4a82e490a25b899e
Strykar1 <ahmedms.ftw@gmail.com>
2019-09-20 00:17:09 +0200

    fix: player can now collide

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private FloatingJoystick joystick = null;
-    [SerializeField] private float speed = 0.12f;
-    [SerializeField] private Rigidbody Rigidbody = null;
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private Rigidbody rigidbody = null;
 
     // Start is called before the first frame update
     void Start() {
     }
 
     // Update is called once per frame
-    void Update() {
-        Rigidbody.velocity = new Vector3(-joystick.Horizontal * 100f * speed,
-            Rigidbody.velocity.y,
-            -joystick.Vertical * 100f * speed);
+    void FixedUpdate() {
+        rigidbody.MovePosition(rigidbody.position + new Vector3(-joystick.Horizontal * speed * Time.deltaTime,
+            0,
+            -joystick.Vertical * speed * Time.deltaTime));
     }
 }
