60a64fcd2eafd27ae0ceb3dc6a8c4fc80bcd069d
Strykar1 <ahmedms.ftw@gmail.com>
2019-09-22 22:30:02 +0200

    refactor: mesh colliders changed to box colliders

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private FloatingJoystick joystick = null;
     [SerializeField] private float speed = 12f;
-    [SerializeField] private Rigidbody rigidbody = null;
+    [SerializeField] private Rigidbody rbody = null;
 
     // Start is called before the first frame update
     void Start() {
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate() {
-        rigidbody.MovePosition(rigidbody.position + new Vector3(-joystick.Horizontal * speed * Time.deltaTime,
+        rbody.MovePosition(rbody.position + new Vector3(-joystick.Horizontal * speed * Time.deltaTime,
             0,
             -joystick.Vertical * speed * Time.deltaTime));
     }
